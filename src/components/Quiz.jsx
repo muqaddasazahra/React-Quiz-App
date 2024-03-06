bbd7bcc8359135bc2b90ad6f89ae8fab4dec9536
muqaddasazahra <muqaddasazahra@gmail.com>
2024-03-06 14:18:39 +0500

    shuffled answers & added quiz ending screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import imgQuizCompleted from '../assets/quiz-complete.png';
 
 export default function Quiz()
 {
 const [userAnswers, setUserAnswers]=useState([]);
 const activeQuestionIndex=userAnswers.length;
+const quizIsComplete= QUESTIONS.length===activeQuestionIndex;
+
+if(quizIsComplete)
+{
+    return <div id="summary">
+        <img src={imgQuizCompleted} alt="" />
+        <h2>Quiz Completed!</h2>
+    </div>
+}
 
 function handleClickAnswer(answer)
 {
@@ -14,11 +24,14 @@ setUserAnswers((prevAnswers)=>
 })
 }
 
+const shuffledAnswers=[...QUESTIONS[activeQuestionIndex].answers];
+shuffledAnswers.sort(()=>Math.random()-0.5)
+
 return <div id="quiz">
     <div id="question">
     <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
     <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer)=>
+        {shuffledAnswers.map((answer)=>
         <li key={answer} className="answer">
             <button onClick={()=>handleClickAnswer(answer)}>
                 {answer}
